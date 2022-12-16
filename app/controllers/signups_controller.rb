4c35ba884b6faceeebadcc367035579626ca4d89
Justin Woodruff <justin.dwayne.woodruff@gmail.com>
2022-12-16 10:13:21 -0600

    commit tree c4424e491a7c7064335d3232522f45c91a979521

---
+++ app/controllers/signups_controller.rb
@@ -0,0 +1,18 @@
+class SignupsController < ApplicationController
+    def create
+        signup = Signup.create(signup_params)
+        if signup
+            render json: signup, status: :created
+        else
+            render json: {error: "validation errors"}, status: :unprocessable_entity
+        end
+
+    end
+
+    private
+
+    def signup_params
+        params.permit(:time, :camper_id, :activity_id)
+    end
+
+end
